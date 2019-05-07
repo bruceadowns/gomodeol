ba89b608adfbf00201e83a0f71cd75cc8468d193
downsb <bruce.downs@autodesk.com>
2019-05-07 09:03:04 -0700

    Add glass method crlf

---
+++ glass.go
@@ -0,0 +1,8 @@
+package gomodeol
+
+import "rsc.io/quote"
+
+// Glass ...
+func Glass() string {
+	return quote.Glass()
+}
---
+++ glass_test.go
@@ -0,0 +1,10 @@
+package gomodeol
+
+import "testing"
+
+func TestGlass(t *testing.T) {
+	want := "I can eat glass and it doesn't hurt me."
+	if got := Glass(); got != want {
+		t.Errorf("Glass() = %q, want %q", got, want)
+	}
+}
